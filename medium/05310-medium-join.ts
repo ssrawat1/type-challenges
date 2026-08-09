7eb7b8b94bf370c72989f7f87f30abd692edff4a
ssrawat1 <ssr911999@gmail.com>
2026-08-09 14:18:33 +0530

    solved the array.join methods using the types

--- medium/05310-medium-join.ts
+++ medium/05310-medium-join.ts
@@ -19,7 +19,9 @@
 
 /* _____________ Your Code Here _____________ */
 
-type Join<T, U> = any
+type Join<T extends any[], U extends string | number = ","> = T extends [infer F, ...infer R] ? `${F & string}${R extends [] ? "" : U}${Join<R, U>}` : ""
+
+type T = Join<['1', '1', '1']>
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
