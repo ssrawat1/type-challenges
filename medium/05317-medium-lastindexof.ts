2d74c224df0560229e5f0b870ac8a24e7a8a8bf9
ssrawat1 <ssr911999@gmail.com>
2026-08-10 09:42:01 +0530

    solved the lastIndexOf method of JS using types

--- medium/05317-medium-lastindexof.ts
+++ medium/05317-medium-lastindexof.ts
@@ -19,10 +19,17 @@
 
 /* _____________ Your Code Here _____________ */
 
-type LastIndexOf<T, U> = any
+type IsEqual<X, Y> = (<T>() => T extends X ? true : false) extends (<T>() => T extends Y ? true : false) ? true : false
+
+type ReverseArray<T extends any[]> = T extends [infer F, ...infer R] ? [...ReverseArray<R>, F] : T
+
+type LastIndexOf<T extends any[], U, I extends number = -1> = ReverseArray<T> extends [infer F, ...infer R] ? (IsEqual<F, U> extends true ? I : LastIndexOf<R, U, R["length"]>) : -1
+
+type T = LastIndexOf<[0, 0, 0], 2>
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
+import { reverse } from 'dns';
 
 type cases = [
   Expect<Equal<LastIndexOf<[1, 2, 3, 2, 1], 2>, 3>>,
