8280a9e7dae4eaa0bd967e8a8d1ac35c20806892
ssrawat1 <ssr911999@gmail.com>
2026-08-14 11:26:41 +0530

    Write the type level logic to find the first unique character index

--- medium/09286-medium-firstuniquecharindex.ts
+++ medium/09286-medium-firstuniquecharindex.ts
@@ -12,7 +12,18 @@
 
 /* _____________ Your Code Here _____________ */
 
-type FirstUniqueCharIndex<T extends string> = any
+// type BuildCharArray<T extends string, A extends string[] = []> = T extends `${infer F}${infer R}` ? BuildCharArray<R, [...A, F]> : A
+
+type FirstUniqueCharIndex<T extends string, A extends any[] = [], R extends string[] = [], index extends number[] = []> =
+  T extends `${infer F}${infer Rest}`
+  ? F extends A[number]
+  ? FirstUniqueCharIndex<Rest, [...A, F], [...R, F]>
+  : FirstUniqueCharIndex<Rest, [...A, F], R>
+  : A extends [infer F, ...infer Rest]
+  ? F extends R[number]
+  ? FirstUniqueCharIndex<T, Rest, R, [...index, 0]>
+  : index['length']
+  : -1
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
