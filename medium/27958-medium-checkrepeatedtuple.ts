06092ac1368eb2cf5945aa8c4b6600e82e59dad7
ssrawat1 <ssr911999@gmail.com>
2026-08-23 16:08:07 +0530

    write the logic to check repeated character existnece in tuple

--- medium/27958-medium-checkrepeatedtuple.ts
+++ medium/27958-medium-checkrepeatedtuple.ts
@@ -19,7 +19,12 @@
 
 /* _____________ Your Code Here _____________ */
 
-type CheckRepeatedTuple<T extends unknown[]> = any
+type IsEqual<X, Y> = (<T>() => T extends X ? 1 : 0) extends <T>() => T extends Y ? 1 : 0 ? true : false;
+
+type Check<F, T> = T extends [infer F1, ...infer R] ? IsEqual<F, F1> extends true ? true : Check<F, R> : false
+
+type CheckRepeatedTuple<T extends unknown[]> = T extends [infer F, ...infer R] ? Check<F, R> extends true
+  ? true : CheckRepeatedTuple<R> : false
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
