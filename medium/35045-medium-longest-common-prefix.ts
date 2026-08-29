055de947cc2f33e485ea83ce699a09cb7676abae
ssrawat1 <ssr911999@gmail.com>
2026-08-29 12:35:55 +0530

    Write the logic to find the longest common prefix

--- medium/35045-medium-longest-common-prefix.ts
+++ medium/35045-medium-longest-common-prefix.ts
@@ -25,7 +25,25 @@
 
 /* _____________ Your Code Here _____________ */
 
-type LongestCommonPrefix<T extends string[], P extends string = ''> = any
+type CheckLongestCommonPrefix<S1 extends string, S2 extends string> =
+  S1 extends `${infer F1}${infer R1}`
+  ? S2 extends `${infer F2}${infer R2}`
+  ? F1 extends F2
+  ? `${F1}${CheckLongestCommonPrefix<R1, R2>}`
+  : "" :
+  "" :
+  "";
+
+
+type LongestCommonPrefix<T extends string[], P extends string = ''> =
+  P extends ""
+  ? T extends [infer F extends string, ...infer R extends string[]]
+  ? F extends "" ? "" : LongestCommonPrefix<R, F>
+  : ""
+  : T extends [infer F1 extends string, ...infer R1 extends string[]]
+  ? CheckLongestCommonPrefix<P, F1> extends "" ? ""
+  : LongestCommonPrefix<R1, CheckLongestCommonPrefix<P, F1>> : P
+
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
