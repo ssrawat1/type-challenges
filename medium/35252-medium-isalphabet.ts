f22763ed5f58a2022c89e1acf86d3ecf83c03fc4
ssrawat1 <ssr911999@gmail.com>
2026-08-29 13:17:27 +0530

    write the logic to check IsAlphabet or not

--- medium/35252-medium-isalphabet.ts
+++ medium/35252-medium-isalphabet.ts
@@ -12,7 +12,20 @@
 
 /* _____________ Your Code Here _____________ */
 
-type IsAlphabet<S extends string> = any
+type Lowercase_Char = [
+  'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+  'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+]
+
+type Uppercase_Char = [
+  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+  'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+]
+
+type Alphabets = [...Lowercase_Char, ...Uppercase_Char]
+
+
+type IsAlphabet<S extends string> = S extends Alphabets[number] ? true : false
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
